a50bc323dfef248ee356b22abf25245852957120
Ali Vidler <alisonvidler@gmail.com>
2025-07-21 15:58:23 -0400

    Fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Marriageparty from './Marriageparty.jsx'
-import Marriageparty from '../../src/Marriageparty.jsx' // Adjusted import path
+
+
 
 function App() {
   const [count, setCount] = useState(0)
